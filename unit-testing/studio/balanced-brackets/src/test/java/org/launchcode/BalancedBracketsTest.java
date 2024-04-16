45524ee5ad41f90df91d3bbdc22ea0eb3ac3cffc
AishuSenthil <124016737+AishuSenthil@users.noreply.github.com>
2024-04-15 20:30:10 -0500

    Studio

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -6,8 +6,70 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BalancedBracketsTest {
     //TODO: add tests here
+
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void onlyBracketsRetrunsOne(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void BracketReturnTrueTwo(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(("[LaunchCode]")));
+    }
+
+    @Test
+    public void BracketReturnTrueThree(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void BracketReturnTrueFour(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void BracketReturnsTrueFive(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void BracketReturnsTrueSix(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void BracketReturnFalseOne(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void BracketReturnFalseTwo(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void BracketReturnFalseThree(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void BracketReturnsFalseFour(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void BracketReturnsFalseFive(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void BracketReturnsFalseSix(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
 }
+
+
